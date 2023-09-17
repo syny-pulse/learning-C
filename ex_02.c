583da5cbebb962a92a24097b434afd1b7eb233ed
syny-pulse <sinaijose002@gmail.com>
2023-09-17 23:13:45 +0300

    Further learning

--- ex_02.c
+++ ex_02.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
-int age;
-int *address = &age;
-*address = 37;
-printf("%u", *address);
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+	int n,m;
+    double p,q;
+    int sum1, diff1;
+    double sum2, diff2;
+    scanf("%d %d", &n, &m);
+    scanf("%lf %lf", &p, &q);
+    sum1 = n + m;
+    diff1 = n - m;
+    sum2 = p + q;
+    diff2 = p - q;
+    printf("%d %d\n", sum1, diff1);
+    printf("%.1lf %.1lf\n", sum2, diff2);
+    return 0;
+}
